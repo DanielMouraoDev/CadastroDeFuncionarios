c439a109cc1d0047040fe61b2c67f2806f63dd2a
DanielMouraoDev <danielmourao777@gmail.com>
2025-10-13 15:23:03 -0300

    feat: Cria o primeiro  Controller colocando as anotaçoes  @RequestController e @RequestMapping e cria uma rota de boas vindas usando @GetMapping

--- src/main/java/dev/Daniel/CadastroDeFuncionarios/Controller.java
+++ src/main/java/dev/Daniel/CadastroDeFuncionarios/Controller.java
@@ -1,4 +1,15 @@
 package dev.Daniel.CadastroDeFuncionarios;
 
-public class controller {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasvindas () {
+            return "Essa é a minha primeira menssagem na rota";
+    }
 }
