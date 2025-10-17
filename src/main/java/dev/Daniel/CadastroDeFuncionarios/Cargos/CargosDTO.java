ffe1febfc85899c0162baac229eaa7822c3e2937
DanielMouraoDev <danielmourao777@gmail.com>
2025-10-17 16:18:20 -0300

    Chore: Inclui a versao do DTO a entidade que mapeaia manualmente no Funcionario e cargo mapper os caminhos entidades x dto

--- src/main/java/dev/Daniel/CadastroDeFuncionarios/Cargos/CargosDTO.java
+++ src/main/java/dev/Daniel/CadastroDeFuncionarios/Cargos/CargosDTO.java
@@ -1,4 +1,15 @@
 package dev.Daniel.CadastroDeFuncionarios.Cargos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CargosDTO {
+
+    private Long id;
+    private String Cargo;
+    private int salario;
 }
--- src/main/java/dev/Daniel/CadastroDeFuncionarios/Cargos/CargosMapper.java
+++ src/main/java/dev/Daniel/CadastroDeFuncionarios/Cargos/CargosMapper.java
@@ -1,4 +1,28 @@
 package dev.Daniel.CadastroDeFuncionarios.Cargos;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CargosMapper {
+
+    public CargosModel map(CargosDTO cargosDTO) {
+
+        CargosModel cargosModel = new CargosModel();
+        cargosModel.setId(cargosDTO.getId());
+        cargosModel.setCargo(cargosDTO.getCargo());
+        cargosModel.setSalario(cargosDTO.getSalario());
+
+        return cargosModel;
+    }
+
+    public CargosDTO map(CargosModel cargosModel) {
+
+        CargosDTO cargosDTO = new CargosDTO();
+        cargosModel.setId(cargosModel.getId());
+        cargosModel.setCargo(cargosModel.getCargo());
+        cargosModel.setSalario(cargosDTO.getSalario());
+
+        return cargosDTO;
+    }
+
 }
