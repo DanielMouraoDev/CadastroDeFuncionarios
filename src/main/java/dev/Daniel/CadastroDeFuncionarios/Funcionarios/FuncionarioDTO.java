d331796a6e31a0fa57e553b21b0e8c6f49176cc1
DanielMouraoDev <danielmourao777@gmail.com>
2025-10-17 16:18:20 -0300

    Chore: Inclui a versao do DTO a entidade que mapeaia manualmente no Funcionario e cargo mapper os caminhos entidades x dto

--- src/main/java/dev/Daniel/CadastroDeFuncionarios/Funcionarios/FuncionarioDTO.java
+++ src/main/java/dev/Daniel/CadastroDeFuncionarios/Funcionarios/FuncionarioDTO.java
@@ -1,4 +1,20 @@
 package dev.Daniel.CadastroDeFuncionarios.Funcionarios;
 
+import dev.Daniel.CadastroDeFuncionarios.Cargos.CargosModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FuncionarioDTO {
+
+    private long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String senioridade;
+    private CargosModel cargo;
+
 }
